cf3109ad8180c967fe60dc4ebeeb059f6dc79982
nickmilikich <nickmilikich@gmail.com>
2020-11-15 18:32:08 -0500

    Simplified syntax

--- DialysisCare.R
+++ DialysisCare.R
@@ -13,7 +13,7 @@ dial$TotalStations = as.integer(dial$TotalStations)
 dial$ZipCode = as.factor(dial$ZipCode)
 
 dial.complete = dial
-dial = dial[,c(3,4,5,6,7,8,9,10,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,48,49,50,51,52,53,55,56,57,58,59)]
+dial = dial[,c(3:10,14,15:53,55:59)]
 
 ###############################
 # First iteration - cutoff of 1
